854ede88e8290e37306cd87f86b25c505f04eecf
nikolagereci <37083905+nikolagereci@users.noreply.github.com>
2022-05-26 14:14:10 +0200

    python medjufaza

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
-let reading = 0
 led.setBrightness(8)
-basic.forever(function () {
+basic.forever(function on_forever() {
     pins.analogWritePin(AnalogPin.P0, 1023)
-    reading = pins.analogReadPin(AnalogPin.P1)
+    let reading = pins.analogReadPin(AnalogPin.P1)
+    queue.push(Math.trunc(reading))
     pins.analogWritePin(AnalogPin.P0, 0)
-    basic.showString("" + (reading))
+    basic.showString("" + ("" + reading))
     basic.pause(5000)
 })
+// pushes into queue of defined len, returns queue avg
+let queue = [parseInt]
+function normalise(x: number): number {
+    return 5 * x
+}
+
