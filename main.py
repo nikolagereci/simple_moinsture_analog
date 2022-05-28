bce4543667f17fe8706c0f5c4e9c7d471e9250c9
nikolagereci <37083905+nikolagereci@users.noreply.github.com>
2022-05-28 17:43:56 +0200

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -5,24 +5,7 @@ led.set_brightness(8)
 def on_forever():
     pins.analog_write_pin(AnalogPin.P0, 1023)
     reading = pins.analog_read_pin(AnalogPin.P1)
-    queue.push(int(reading))
     pins.analog_write_pin(AnalogPin.P0, 0)
     basic.show_string("" + str((reading)))
     basic.pause(5000)
 basic.forever(on_forever)
-
-#pushes into queue of defined len, returns queue avg
-queue = deque('asd')
-LEN = 20
-def normalise(reading):
-    if queue.length < LEN:
-        queue.apend(reading)
-    else:
-        queue.(None)
-
-
-
-    
-  queue.push(reading)
-  queue = [int]
-  return 5 * x
--- main.ts
+++ main.ts
@@ -1,15 +1,35 @@
 led.setBrightness(8)
+
+class Normalizer{
+    size: number;               //field length
+    normalized_val: number;     //calculated normalized value
+    Q: number[];
+
+    constructor(){
+        this.size = 30;
+        this.normalized_val = 0;
+        this.Q.fill(1023);
+    }
+
+    ///add a reading and return the normalized value 
+    add_reading(x: number){
+        if (this.Q.length == this.size){
+            this.Q.shift();
+        }
+        this.Q.push(x);
+        let sum = 0;
+        this.Q.forEach(function(item, index){
+            sum += item;
+        });
+        this.normalized_val = sum / this.Q.length;
+        return this.normalized_val;
+    }
+}
+
 basic.forever(function on_forever() {
     pins.analogWritePin(AnalogPin.P0, 1023)
     let reading = pins.analogReadPin(AnalogPin.P1)
-    queue.push(Math.trunc(reading))
     pins.analogWritePin(AnalogPin.P0, 0)
     basic.showString("" + ("" + reading))
     basic.pause(5000)
 })
-// pushes into queue of defined len, returns queue avg
-let queue = [parseInt]
-function normalise(x: number): number {
-    return 5 * x
-}
-
