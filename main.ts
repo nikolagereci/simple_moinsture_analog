385680eff48ac5043004db196a8355f86c4bef4a
nikolagereci <37083905+nikolagereci@users.noreply.github.com>
2022-05-28 18:31:41 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
 led.setBrightness(8)
 
 class Normalizer{
-    size: number;               //field length
-    normalized_val: number;     //calculated normalized value
-    Q: number[];
+    private size: number;               //field length
+    private normalized_val: number;     //calculated normalized value
+    private Q: number[];
 
-    constructor(){
-        this.size = 30;
+    constructor(s: number){
+        this.size = s;
         this.normalized_val = 0;
-        this.Q.fill(1023);
+        for (let i=0; i < this.size; i++){
+            this.Q[i] = 1023;
+        }
     }
 
     ///add a reading and return the normalized value 
@@ -26,10 +28,44 @@ class Normalizer{
     }
 }
 
-basic.forever(function on_forever() {
-    pins.analogWritePin(AnalogPin.P0, 1023)
-    let reading = pins.analogReadPin(AnalogPin.P1)
-    pins.analogWritePin(AnalogPin.P0, 0)
-    basic.showString("" + ("" + reading))
-    basic.pause(5000)
-})
+namespace watering {
+
+    let threshold = 800;
+    let watering_period = 60000; //default watering period is one minute
+    let pause_period = 5000;
+    let normalizer: Normalizer;
+    export function init(){
+        normalizer = new Normalizer(30);
+    }
+
+    function read_moisture_sensor() {
+        pins.analogWritePin(AnalogPin.P0, 1023);
+        let reading = pins.analogReadPin(AnalogPin.P1);
+        //dry soil should cause reduced value
+        pins.analogWritePin(AnalogPin.P0, 0);
+        basic.showString("R" + reading.toString());
+        let normalized_val = normalizer.add_reading(reading);
+        basic.pause(pause_period);
+        basic.showString("N" + normalized_val.toString());
+        return normalized_val;
+    }
+
+    function water() {
+        pins.digitalWritePin(DigitalPin.P1, 0);
+        basic.pause(watering_period);
+        pins.digitalWritePin(DigitalPin.P1, 1);
+    }
+
+    basic.forever(function on_forever() {
+        //turn off pump by default
+        pins.digitalWritePin(DigitalPin.P1, 1);
+
+        let normalized_val = read_moisture_sensor();
+        if (normalized_val < threshold) {
+            water();
+        }
+    })
+}
+
+
+
